437ea16e8303c3df6a476310e279ca8bcd7a151e
Thomas Colin <th.colin3@gmail.com>
2017-02-07 12:09:46 +0100

    Add construct test for Torrent

--- tests/TorrentTest.php
+++ tests/TorrentTest.php
@@ -8,7 +8,14 @@ class TorrentTest extends PHPUnit_Framework_TestCase{
     error_reporting(E_ALL^E_WARNING);
     ini_set('memory_limit', '-1');
   }
-  
+
+  public function testConstructWithTrackerSuccess(){
+    $torrent = new Torrent('T411');
+
+    $this->assertInstanceOf('thcolin\TorrentDjinn\Torrent', $torrent);
+    $this->assertEquals('T411', $torrent->getTracker());
+  }
+
 }
 
 ?>
